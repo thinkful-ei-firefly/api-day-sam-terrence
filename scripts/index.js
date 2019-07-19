1d57455328dc55c3477931bf1f109ff133f42a03
Ramarduk <ramarduk@gmail.com>
2019-07-19 10:02:42 -0600

    step 4 complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
 'use strict';
-/* global shoppingList, store, Item, api */
+/* global shoppingList, store, Item, api, $ */
 // eslint-disable-next-line no-unused-vars
 $(document).ready(function() {
   shoppingList.bindEventListeners();
-  shoppingList.render();
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
 });
 
-store.items.push(Item.create('apples'));
+//store.items.push(Item.create('apples'));
 
 // api.getItems()
 //   .then(response =>response.json())
